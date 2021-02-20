bb0276116183713013f4a85960cf69bb47e9f020
Raynur <kinespaev.raynur@gmail.com>
2021-02-20 21:59:50 +0600

    123

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -14,6 +14,10 @@ import javax.script.ScriptException;
 public class MainActivity extends AppCompatActivity {
 
     private EditText display;
+
+    public String Memory;
+    public String resultString;
+    public String text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,18 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
         display = findViewById(R.id.input);
         display.setShowSoftInputOnFocus(false);
-
-        display.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (getString(R.string.display).equals(display.getText().toString())) {
-                    display.setText("");
-                }
-            }
-        });
     }
 
-    private void updateText(String strToAdd){
+    public void updateText(String strToAdd, int indent){
         String oldStr = display.getText().toString();
         int cursorPos = display.getSelectionStart();
         String leftStr = oldStr.substring(0, cursorPos);
@@ -41,95 +36,148 @@ public class MainActivity extends AppCompatActivity {
             display.setText(strToAdd);
             display.setSelection(cursorPos + 1);
         }
-        else{
+        else if(indent==1){
             display.setText(String.format("%s%s%s", leftStr, strToAdd, rightStr));
             display.setSelection(cursorPos + 1);
+        }
+        else if(indent==2){
+            display.setText(String.format("%s%s%s", leftStr, strToAdd, rightStr));
+            display.setSelection(cursorPos + 2);
+        }
+        else{
+            display.setText(String.format("%s%s%s", leftStr, strToAdd, rightStr));
+            display.setSelection(cursorPos + indent);
         }
     }
 
     public void zeroBTN(View view){
-        updateText("0");
+        updateText("0", 1);
     }
 
     public void oneBTN(View view){
-        updateText("1");
+        updateText("1", 1);
     }
 
     public void twoBTN(View view){
-        updateText("2");
+        updateText("2", 1);
     }
 
     public void threeBTN(View view){
-        updateText("3");
+        updateText("3", 1);
     }
 
     public void fourBTN(View view){
-        updateText("4");
+        updateText("4", 1);
     }
 
     public void fiveBTN(View view){
-        updateText("5");
+        updateText("5", 1);
     }
 
     public void sixBTN(View view){
-        updateText("6");
+        updateText("6", 1);
     }
 
     public void sevenBTN(View view){
-        updateText("7");
+        updateText("7", 1);
     }
 
     public void eightBTN(View view){
-        updateText("8");
+        updateText("8", 1);
     }
 
     public void nineBTN(View view){
-        updateText("9");
+        updateText("9", 1);
     }
 
     public void pointBTN(View view){
-        updateText(".");
+        updateText(".", 1);
     }
 
     public void addBTN(View view){
-        updateText("+");
+        updateText("+", 1);
     }
 
     public void substractBTN(View view){
-        updateText("-");
+        updateText("-", 1);
     }
 
     public void multiplyBTN(View view){
-        updateText("*");
+        updateText("*", 1);
     }
 
-    public void divideBTN(View view){
-        updateText("/");
-    }
+    public void divideBTN(View view) { updateText("/", 1); }
 
     public void equalsBTN(View view) throws ScriptException {
         //display.setText(engine.eval(text).toString());
         String s = display.getText().toString();
-        display.setText(calc(s));
+        s = s.replaceAll("√" , "Math.sqrt");
+        double resultDouble = calc(s);
+        if (resultDouble % 1 == 0) {
+            int resultInt = (int) resultDouble;
+            resultString = Integer.toString(resultInt);
+        }
+        else{
+            resultString = Double.toString(resultDouble);
+        }
+        display.setText("");
+        updateText(resultString, resultString.length());
+        //display.setText(resultString);
     }
 
-    public String calc(String input) throws ScriptException {
+    public double calc(String input) throws ScriptException {
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine engine = mgr.getEngineByName("rhino");
-        Object o = engine.eval("1+1");
-        return engine.eval(input).toString();
+        return (Double)engine.eval(input);
     }
 
     public void plusMinusBTN(View view){
+        StringBuilder builder = new StringBuilder(display.getText());
+        String oldStr = display.getText().toString();
+        int cursorPos = display.getSelectionStart();
+        String leftStr = oldStr.substring(0, cursorPos);
+        String rightStr = oldStr.substring(cursorPos);
+        final boolean b = leftStr.lastIndexOf("+") != -1 || leftStr.lastIndexOf("-") != -1;
+        if(b && leftStr.lastIndexOf("+")>leftStr.lastIndexOf("-")){
+            if(leftStr.lastIndexOf("+") != -1) {
+                int index = builder.lastIndexOf("+", cursorPos - 1);
+                builder.replace(index, index+1, "-");
+                display.setText("");
+                updateText(builder.toString(), leftStr.length());
+            }
+            else if(leftStr.lastIndexOf("-") != -1){
+                int index = builder.lastIndexOf("-", cursorPos - 1);
+                builder.replace(index, index+1, "+");
+                display.setText("");
+                updateText(builder.toString(), leftStr.length());
+            }
+        }
+        else if(b && leftStr.lastIndexOf("+")<leftStr.lastIndexOf("-")){
+            if(leftStr.lastIndexOf("-") != -1){
+                int index = builder.lastIndexOf("-", cursorPos - 1);
+                builder.replace(index, index+1, "+");
+                display.setText("");
+                updateText(builder.toString(), leftStr.length());
+            }
+            else if(leftStr.lastIndexOf("+") != -1) {
+                int index = builder.lastIndexOf("+", cursorPos - 1);
+                builder.replace(index, index+1, "-");
+                display.setText("");
+                updateText(builder.toString(), leftStr.length());
+            }
+        }
 
     }
 
     public void rootBTN(View view){
-
+        updateText("√()", 2);
     }
 
     public void onedivideXBTN(View view){
-
+        text = display.getText().toString();
+        display.setText("");
+        resultString = "1/("+text+")";
+        updateText(resultString, resultString.length());
     }
 
     public void clearTN(View view){
@@ -149,26 +197,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void MRBTN(View view){
-
+    public void MRBTN(View view) throws ScriptException {
+        if(Memory != null){
+            double resultDouble;
+            resultDouble = calc(Memory);
+            if (resultDouble % 1 == 0) {
+                int resultInt = (int) resultDouble;
+                Memory = Integer.toString(resultInt);
+            }
+            else{
+                Memory = Double.toString(resultDouble);
+            }
+                updateText(Memory, Memory.length());
+        }
     }
 
     public void MCBTN(View view){
-
+        Memory = null;
     }
 
-    public void MSBTN(View view){
-
+    public void MSBTN(View view) throws ScriptException {
+        Memory = display.getText().toString();
     }
 
     public void MplusBTN(View view){
-
+        if(Memory != null)
+            Memory=Memory + "+(" + display.getText().toString() + ")";
     }
 
     public void MminusBTN(View view){
-
+        if(Memory != null)
+            Memory=Memory + "-(" + display.getText().toString() + ")";
     }
-
-
-
 }
