678d324b2f7f7b62357b4cf6a445bd26cd930759
Raynur <kinespaev.raynur@gmail.com>
2021-02-23 12:19:13 +0600

    3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
--- src/com/company/Operation.java
+++ src/com/company/Operation.java
@@ -1,5 +1,6 @@
 package com.company;
 
+
 public class Operation {
 
     private Account debet;
